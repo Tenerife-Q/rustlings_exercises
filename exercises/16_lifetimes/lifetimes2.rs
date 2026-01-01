c446709c455ba629dfda731431369bad6429fcdb
Tenerife-Q <2892015852@qq.com>
2026-01-01 16:04:14 +0800

    lifetimes annotation

--- exercises/16_lifetimes/lifetimes2.rs
+++ exercises/16_lifetimes/lifetimes2.rs
@@ -9,10 +9,13 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     // TODO: Fix the compiler error by moving one line.
+    // 报错原因: string1 的所有权在 main 函数中，而 result 的生命周期超出了 string1 的作用域。
+    // 解决方法: 将 string1 的声明移动到 result 使用之前，确保 string1 在 result 使用时仍然有效。
 
     let string1 = String::from("long string is long");
     let result;
     
+    // string2 函数作用域结束后，result 仍然引用 string1，因此不会报错。
     let string2 = String::from("xyz");
     result = longest(&string1, &string2);
     
--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,6 +1,9 @@
 // Lifetimes are also needed when structs hold references.
 
 // TODO: Fix the compiler errors about the struct.
+// 为结构体添加生命周期注解
+// 结构体 Book 中的字段 author 和 title 都是字符串切片引用(&str)，
+// 需要为结构体添加生命周期注解，确保引用在结构体实例存在期间是有效的。
 
 struct Book<'a> {
     author: &'a str,
