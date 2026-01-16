10a4343221e8c427cf2d4fb3343f9d3c38700faa
Tenerife-Q <2892015852@qq.com>
2026-01-16 19:16:08 +0800

    up to 20.Threads

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -17,17 +17,28 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
+    
+    // 解决方法：利用 mpsc::Sender 实现了 Clone 的特性
+    // 在进入第一个线程前，克隆一份 tx
+    let tx1 = tx.clone();
+    
+    // 为了让两个线程都能访问 q 的不同部分，我们需要先解构 q，
+    // 否则整个 q 会在第一个线程创建时被 move 进去。
+    let first_vals = q.first_half;
+    let second_vals = q.second_half;
+
     thread::spawn(move || {
-        for val in q.first_half {
+        for val in first_vals {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
 
     thread::spawn(move || {
-        for val in q.second_half {
+        for val in second_vals {
             println!("Sending {val:?}");
+            // 这里直接使用原始的 tx，它被 move 进了第二个线程
             tx.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
